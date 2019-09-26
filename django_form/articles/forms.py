3b48b4b1df6ad20ed6cd118e55d99ad311b06d7c
tesschung <geobera0910@naver.com>
2019-09-26 11:35:34 +0900

    django_form checking validation of user data

--- django_form/articles/forms.py
+++ django_form/articles/forms.py
@@ -2,5 +2,24 @@ from django import forms
 
 # 사용자에게 입력받는 field 작성
 class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=20)
-    content = forms.CharField()
+    title = forms.CharField(
+        max_length=20,
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'Enter the title',
+            },
+        ),
+    )
+
+
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'placeholder':'Enter the content',
+                'rows': 5,
+                'cols': 50,
+            },
+        ),
+    )
